b2f57e9a65f231ce93cab6328c9a8e8ecd594e39
Javier Morales <javiermorales@Javiers-MacBook-Pro.local>
2023-01-22 23:22:25 -0600

    agregando test para students

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -8,9 +8,12 @@ use Tests\TestCase;
 use App\Models\Student;
 use Illuminate\Database\Eloquent\Factories\StudentFactory;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class StudentTest extends TestCase
 {
+
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -29,6 +32,46 @@ class StudentTest extends TestCase
                
         ])->assertStatus(200);
     }
+
+    public function test_StudentList(){
+        // $user = Student::factory()->create();
+         $alumnos = Student::factory()->count(3)->create();
+
+         $response = $this->json('get','api/alumnos')
+            ->assertStatus(200)
+            ->assertJsonStructure([
+               [    "id",
+                    "nombre",
+                    "codigo"
+                ]
+            ])
+            ->assertJsonCount(3);
+
+         $this->assertDatabaseCount('students', 3);
+    }
+
+    public function test_SaveStudent(){
+        $student = [
+
+             'name' => "javier",
+             'code' => "-J123XYZ"
+        ];
+
+
+        $this->json('post', '/api/alumnos', $student)
+            ->assertStatus(201)
+            ->assertJsonStructure([
+                'id',
+                'name',
+                'code',
+            ]);
+
+        $this->assertDatabaseHas('students', [
+                'name' => 'javier',
+                'code' => '-J123XYZ',
+            ]);
+    }
+
 
 
 }
